a5969238fb1b7c4c8462687d6ddcf96ea2cf1cc8
app2641 <app2641@gmail.com>
2021-06-05 19:10:49 +0900

    No referrer

--- src/components/google_play_badge.js
+++ src/components/google_play_badge.js
@@ -6,6 +6,7 @@ export default function GooglePlayBadge() {
     <a
       href="https://play.google.com/store/apps/details?id=com.app2641.moonlovers"
       target="_blank"
+      rel="noreferrer"
     >
       <StaticImage
         src="../../static/images/google-play-badge.png"
