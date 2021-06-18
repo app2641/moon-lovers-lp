85150f87fa7169ae84d72c1db9e99f5e7c372044
app2641 <app2641@gmail.com>
2021-06-18 22:25:35 +0900

    Add description

--- src/components/metadata.js
+++ src/components/metadata.js
@@ -5,6 +5,7 @@ export default function Metada() {
   return (
     <Helmet>
       <title>Moon Lovers - 今夜の月齢</title>
+      <meta content="いまの月齢を確認できます。その上、毎夜10時に今夜の月齢も通知しましょう。" name="description" />
     </Helmet>
   );
 }
