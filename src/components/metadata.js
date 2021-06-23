b60936dd54a8d3fcf39b0d49f97fcf6c821b2a3c
app2641 <app2641@gmail.com>
2021-06-23 22:38:00 +0900

    Google Fonts

--- src/components/metadata.js
+++ src/components/metadata.js
@@ -8,6 +8,9 @@ export default function Metada() {
       <meta charset="UTF-8" />
       <meta content="いまの月齢を確認できます。その上、毎夜10時に今夜の月齢も通知しましょう。" name="description" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Suranna&display=swap" rel="stylesheet" />
     </Helmet>
   );
 }
