2af576bc2e9cee1997aabd13f99ed38d3fce6039
app2641 <app2641@gmail.com>
2021-06-26 14:36:50 +0900

    Set favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,12 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `static/favicon.png`,
+      },
+    },
   ],
   siteMetadata: {
     title: 'Moon Lovers - 月を生活のそばに',
